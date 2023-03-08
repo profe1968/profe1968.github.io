d10ef65b0ce0014b13a436c6ad12dc0499dceebf
Cesar Pereira <64308425+profe1968@users.noreply.github.com>
2023-03-07 22:43:12 -0300

    Add files via upload
    Initial commit

---
+++ nav.js
@@ -0,0 +1,8 @@
+/* estas funciones se utilizan para abrir y cerrar el menu en un movil */
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+}
